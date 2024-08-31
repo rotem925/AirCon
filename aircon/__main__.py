edbf8278fae119366851eee08211a9ad2ad52815
Rotem <rotems@ecb.co.il>
2024-08-31 14:54:50 +0300

    fix: min_temp

--- aircon/__main__.py
+++ aircon/__main__.py
@@ -238,7 +238,7 @@ async def run(parsed_args):
         config['temperature_state_topic'] = mqtt_topics['pub'].format(device.mac_address,
                                                                       topics['temp'])
         config['max_temp'] = '86' if device.is_fahrenheit else '30'
-        config['min_temp'] = '61' if device.is_fahrenheit else '16'
+        config['min_temp'] = '60.8' if device.is_fahrenheit else '16'
 
       mqtt_client.publish(mqtt_topics['discovery'].format(device.mac_address),
                           payload=json.dumps(config),
